0bae22e7da428802e12d8801e016132d68ebe6e3
Julia Goronovich <juliagoronovich@gmail.com>
2020-05-05 15:38:38 +0300

    describe the cat completed

---
+++ Describethecat/src/Cat.java
@@ -0,0 +1,25 @@
+
+public class Cat {
+	      public String breed;
+		  public String gender;
+		  public String color;
+		  public Integer height;
+		  public Integer weight;
+		  public Integer age;
+		  
+		  
+		 
+		   
+			public static void main(String[] args) {
+			Cat Lewis = new Cat ();
+			Lewis.breed = "Ragdoll";
+			Lewis.gender = "Male";
+			Lewis.color = "Mitted";
+			Lewis.height = 20;
+			Lewis.weight = 10;
+			Lewis.age = 3;
+    		System.out.println("I bought the cat Lewis, his breed - " +Lewis.breed+",gender - "+Lewis.gender+",color - "+Lewis.color+",height - "+Lewis.height+",weight - "+Lewis.weight+",age - "+Lewis.age);
+		}
+	  
+
+}
